9f50557c3250a254ad128a86517cee06dea47580
Nihil Pudota <nihil2015@gmail.com>
2026-08-20 14:16:04 -0700

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int[] charS = new int[26];
+        int[] charT = new int[26];
+        for(char c: s.toCharArray()){
+            charS[c - 'a']++;
+        }
+        for(char c: t.toCharArray()){
+            charT[c - 'a']++;
+        }
+        for(int i = 0; i < 26; i++){
+            if(charS[i] != charT[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
